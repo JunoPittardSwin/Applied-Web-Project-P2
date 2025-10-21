c2381a259155f7aaf57a6bd417c479a945915506
Ashlyn R <105928880@student.swin.edu.au>
2025-10-21 14:08:39 +1100

    feat: process EOIs into the DB

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -1,7 +1,6 @@
 <?php declare(strict_types=1);
 
-use function Req\post;
-use function Req\postOrBail;
+use Req\FormContext;
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST')
 {
@@ -13,56 +12,154 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST')
 
 require_once(__DIR__ . '/lib/Req.php');
 
-$jobReferenceId = postOrBail('reference');
+$form = FormContext::fromPostBody();
 
-$firstName = postOrBail('first_name');
-$lastName = postOrBail('last_name');
-$emailAddress = postOrBail('email');
-$phoneNumber = postOrBail('phone');
-$gender = post('gender');
-$dateOfBirthString = post('dob');
-$dateOfBirth = null;
+$jobReferenceId = $form->input(
+	readableName: 'Job Reference ID',
+	key: 'reference',
+	regex: '/^J[0-9]{4}$/',
+	required: true,
+);
 
-if ($dateOfBirthString !== null)
+$firstName = $form->input(
+	readableName: 'First Name',
+	key: 'first_name',
+	required: true,
+	maxLength: 32,
+);
+
+$lastName = $form->input(
+	readableName: 'Last Name',
+	key: 'last_name',
+	required: true,
+	maxLength: 32,
+);
+
+$emailAddress = $form->input(
+	readableName: 'Email Address',
+	key: 'email',
+	filterMode: FILTER_VALIDATE_EMAIL,
+	required: true,
+	maxLength: 64,
+);
+
+$phoneNumber = $form->input(
+	readableName: 'Phone Number',
+	key: 'phone',
+	required: true,
+	minLength: 8,
+	maxLength: 20
+);
+
+$gender = $form->input(
+	readableName: 'Gender',
+	key: 'gender',
+	required: false,
+	maxLength: 16
+);
+
+$dateOfBirth = $form->input(
+	readableName: 'Date of Birth',
+	key: 'dob',
+	required: false,
+	mapValue: fn(string $dob) => new DateTimeImmutable($dob)
+);
+
+$otherSkills = $form->input(
+	readableName: 'Other Skills',
+	key: 'other_skills',
+	required: false
+);
+
+$state = $form->input(
+	readableName: 'State',
+	key: 'state',
+	regex: '/^(VIC|NSW|QLD|NT|WA|SA|TAS|ACT)$/',
+	required: true
+);
+
+$streetAddress = $form->input(
+	readableName: 'Street Address',
+	key: 'street_address',
+	required: false,
+	maxLength: 24
+);
+
+$suburb = $form->input(
+	readableName: 'Suburb',
+	key: 'suburb',
+	required: false,
+	maxLength: 24
+);
+
+$postCode = $form->input(
+	readableName: 'Postcode',
+	key: 'postcode',
+	regex: '/^[0-9]+$/',
+	required: false,
+	maxLength: 4,
+	mapValue: intval(...)
+);
+
+$skills = $form->inputArray(
+	readableName: 'Skills',
+	key: 'skills',
+	required: true,
+	memberRegex: '/^[a-z_]+$/',
+	memberMaxLength: 32,
+	memberMinLength: 1,
+);
+
+if ($form->hasErrors())
 {
-	try
-	{
-		$dateOfBirth = new DateTimeImmutable($dateOfBirthString);
-	}
-	catch (DateMalformedStringException $ex)
-	{
-		http_response_code(400);
-		
-		?>
-		<p>Didn't understand the format you wrote your date of birth in.</p>
-		<p>Try writing it like so: <em>DD/MM/YYYY</em>.</p>
-		
-		<a href="./apply.php">Retry your application?</a>
-		<?php
+	http_response_code(400);
+	?>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Expression of Interest | Watertight CyberSec</title>
+		<link rel="stylesheet" href="./css/styles.css">
+	</head>
+	<body>
+		<?php include(__DIR__ . '/header.inc'); ?>
 
-		exit;
-	}
+		<header id="hero-container">
+			<div id="hero">
+				<h1>Please try again</h1>
+				<p>
+					Thanks for submitting your expression of interest. Unfortunately, there's a few
+					issues with it listed below. Please fix these issues and submit again, we'd
+					love to hear from you!
+				</p>
+			</div>
+		</header>
+
+		<main>
+			<article>
+				<h2>Submission Issues</h2>
+				<ul>
+					<?php foreach ($form->htmlErrorList as $error)
+					{
+						?>
+						<li><?= $error ?></li>
+						<?php
+					}
+					?>
+				</ul>
+
+				<a class="button" href="./apply.php">Retry your application</a>
+			</article>
+		</main>
+
+		<?php include(__DIR__ . '/footer.inc'); ?>
+	</body>
+	</html>
+
+	<?php
+	exit;
 }
-
-$state = postOrBail('state');
-$streetAddress = post('street_address');
-$suburb = post('suburb');
-$postCodeString = post('suburb');
-
-// [reference] => J0123
-// [first_name] => Christopher
-// [last_name] => Null
-// [dob] => 01/01/2000
-// [gender] => male
-// [street_address] => 
-// [suburb] => 
-// [state] => 
-// [postcode] => 
-// [email] => 
-// [phone] => 
-// [other_skills] => 
-
-// ...
 
 require_once(__DIR__ . '/lib/EoiManager.php');
 require_once(__DIR__ . '/settings.php');
@@ -71,5 +168,19 @@ $eoiManager = new EoiManager($db);
 
 $eoiManager->submitEoi(
 	// jobReferenceId: $jobReferenceId,
-	firstName: 
+	firstName: $firstName,
+	lastName: $lastName,
+	emailAddress: $emailAddress,
+	phoneNumber: $phoneNumber,
+	gender: $gender,
+	dateOfBirth: $dateOfBirth,
+	state: $state,
+	streetAddress: $streetAddress,
+	suburb: $suburb,
+	postcode: $postCode,
+	skills: $skills,
+	commentsAndOtherSkills: $otherSkills
 );
+
+echo '<pre>';
+print_r($_POST);
