ffe95bfb51a743bc7662352b8255b246401e106a
Ashlyn R <105928880@student.swin.edu.au>
2025-10-21 14:08:20 +1100

    feat: fancy tooling for parsing requests
    this took a while to write, but I've got an API for it that feels slightly more sane than `filter_var` and `preg_match` spam, so hopefully worth it :)

--- project2/lib/Req.php
+++ project2/lib/Req.php
@@ -5,6 +5,8 @@
  */
 namespace Req;
 
+use Exception;
+
 /**
  * Get the query string parameter with the given name.
  * If not given or blank, returns `null`.
@@ -46,45 +48,334 @@ function post(string $name): ?string
 }
 
 /**
- * Get the POST-body request parameter with the given name, or bail out with a HTTP 400 error.
- *
- * @param string $name
- * @return string
+ * Very fancy form-parsing stuff which made the EOI processing a bit more pleasant to write.
+ * 
+ * Provides a wide array of requirements that can be enforced on the shape of an input dataset, and
+ * allows you to map/deserialize that data.
+ * 
+ * @author ashlyn r
  */
-function postOrBail(string $name): string
+class FormContext
 {
-	$value = post($name);
+	/**
+	 * List of errors with the user's input data. If non-empty, the form data should be considered
+	 * invalid. Formatted as HTML.
+	 * 
+	 * @var string[]
+	 */
+	public array $htmlErrorList = [];
 
-	if ($value === null)
+	/**
+	 * Create a form data context from the given associative array of user input data.
+	 */
+	function __construct(private array $inputArray)
+	{}
+
+	/**
+	 * Create a form data context from the POST request body.
+	 * @return static
+	 */
+	public static function fromPostBody(): static
 	{
-		http_response_code(400);
-		exit;
+		return new static($_POST);
 	}
 
-	return $value;
+	/**
+	 * Whether any errors have been encountered so far in the input data.
+	 */
+	public function hasErrors(): bool
+	{
+		return count($this->htmlErrorList) > 0;
+	}
+
+	/**
+	 * Retrieve a piece of data provided by the user in the form.
+	 * 
+	 * @param string $readableName Human-readable name of this form input
+	 * @param string $key Internal data name of this input
+	 * @param bool $required Whether this input must be provided, or should default to `null`
+	 * @param ?string $regex An optional regular expression the data must pass to be valid
+	 * @param ?int $filterMode An optional `FILTER_*` mode the data must match to be valid
+	 * @param ?int $minLength Optional minimum string length of the data
+	 * @param ?int $maxLength Optional maximum string length of the data
+	 * @param ?callable $mapValue Optional function to map the data into another type, if all the above criteria match. May throw an `Exception` to indicate bad data
+	 * @return mixed The data, or `null` if it failed to match. In the latter case, an error will be added to the error list
+	 */
+	public function input(
+		string $readableName,
+		string $key,
+		bool $required,
+		?string $regex = null,
+		?int $filterMode = null,
+		?int $minLength = null,
+		?int $maxLength = null,
+		?callable $mapValue = null
+	): mixed
+	{
+		$value = $this->inputArray[$key] ?? null;
+
+		try
+		{
+			return mapInputValue(
+				value: $value,
+				required: $required,
+				regex: $regex,
+				filterMode: $filterMode,
+				minLength: $minLength,
+				maxLength: $maxLength,
+				mapValue: $mapValue
+			);
+		}
+		catch (InputException $ex)
+		{
+			$error = 'Your ' . htmlspecialchars($readableName) . ' ' . $ex->messageSuffix();
+
+			if (is_string($value))
+			{
+				$error .= ' You wrote: <q>' . htmlspecialchars(strval($value)) . '</q>';
+			}
+
+			$this->htmlErrorList[] = $error;
+
+			return null;
+		}
+	}
+
+	/**
+	 * Retrieve an array of data provided by the user in the form.
+	 * 
+	 * @param string $readableName Human-readable name of this form input
+	 * @param string $key Internal data name of this input
+	 * @param bool $required Whether this input must be provided, or should default to `null`
+	 * @param ?string $memberRegex An optional regular expression each member must pass to be valid
+	 * @param ?int $memberFilterMode An optional `FILTER_*` mode each member must match to be valid
+	 * @param ?int $memberMinLength Optional minimum string length of each member's data
+	 * @param ?int $memberMaxLength Optional maximum string length of each member's data
+	 * @param ?callable $mapMember Optional function to map each array member into another type, if all the above criteria match. May throw an `Exception` to indicate bad data
+	 * @return ?array The data, or `null` if it failed to match. In the latter case, an error will be added to the error list
+	 */
+	public function inputArray(
+		string $readableName,
+		string $key,
+		bool $required,
+		?string $memberRegex = null,
+		?int $memberFilterMode = null,
+		?int $memberMinLength = null,
+		?int $memberMaxLength = null,
+		?callable $mapMember = null
+	): ?array
+	{
+		return $this->input(
+			readableName: $readableName,
+			key: $key,
+			required: $required,
+			mapValue: function (mixed $array) use(
+				$memberRegex,
+				$memberFilterMode,
+				$memberMinLength,
+				$memberMaxLength,
+				$mapMember
+			): ?array
+			{
+				if (!is_array($array))
+				{
+					throw new InputMapFailedException('should be a list of items.');
+				}
+
+				$outArray = [];
+
+				/** @var string[] */
+				$htmlErrorList = [];
+
+				foreach ($array as $member)
+				{
+					try
+					{
+						$outArray[] = mapInputValue(
+							value: $member,
+							required: false,
+							regex: $memberRegex,
+							filterMode: $memberFilterMode,
+							minLength: $memberMinLength,
+							maxLength: $memberMaxLength,
+							mapValue: $mapMember
+						);
+					}
+					catch (InputException $ex)
+					{
+						$htmlErrorList []= sprintf('<li><q>%s</q> %s</li>',
+							htmlspecialchars($member),
+							htmlspecialchars($ex->messageSuffix())
+						);
+
+						continue;
+					}
+				}
+				
+				if (count($htmlErrorList) > 0)
+				{
+					$errorListHtml = implode($htmlErrorList);
+
+					throw new InputMapFailedException(sprintf(
+						"has %u incorrect entries: <ul>%s</ul>",
+						count($htmlErrorList),
+						$errorListHtml
+					));
+				}
+
+				return $outArray;
+			}
+		);
+	}
 }
 
-class Session
+/**
+ * Map a user-given input value based on some number of criteria.
+ * 
+ * Used as the basis for FormContext.
+ * 
+ * @throws InputException If the criteria failed to match
+ */
+function mapInputValue(
+	mixed $value,
+	bool $required,
+	?string $regex = null,
+	?int $filterMode = null,
+	?int $minLength = null,
+	?int $maxLength = null,
+	?callable $mapValue = null
+): mixed
 {
-	/**
-	 * Get session information with the given name. If that data is unset, returns `null`.
-	 *
-	 * @param string $name
-	 * @return mixed
-	 */
-	static function get(string $name): mixed
+	if (is_string($value))
 	{
-		return $_SESSION[$name] ?? null;
+		$value = mb_trim($value);
 	}
 
-	/**
-	 * Set the session information for `$name` to `$data`.
-	 *
-	 * @param string $name The name of the entry in the session info
-	 * @param mixed $data The data to store for that key
-	 */
-	static function set(string $name, mixed $data)
+	if ($value === null || $value === '')
 	{
-		$_SESSION[$name] = $data;
+		if ($required)
+		{
+			throw new InputRequiredException;
+		}
+
+		return null;
+	}
+
+	if ($filterMode !== null)
+	{
+		$value = filter_var($value, $filterMode);
+	}
+
+	if (is_string($value) && ($maxLength !== null || $minLength !== null))
+	{
+		$length = mb_strlen($value);
+
+		if ($minLength !== null && ($length < $minLength))
+		{
+			throw new InputTooShortException($minLength);
+		}
+
+		if ($maxLength !== null && ($length > $maxLength))
+		{
+			throw new InputTooLongException($maxLength);
+		}
+	}
+
+	if (($value === false) || ($regex !== null && preg_match($regex, $value) === false))
+	{
+		throw new InputBadFormatException;
+	}
+
+	if ($mapValue === null)
+	{
+		if (is_string($value))
+		{
+			return mb_trim($value);
+		}
+
+		return $value;
+	}
+
+	try
+	{
+		return $mapValue($value);
+	}
+	catch (\Exception $inner)
+	{
+		if ($inner instanceof InputMapFailedException)
+		{
+			throw $inner;
+		}
+
+		throw new InputMapFailedException(previous: $inner);
+	}
+}
+
+abstract class InputException extends Exception
+{
+	public abstract function messageSuffix(): string;
+}
+
+class InputRequiredException extends InputException
+{
+	#[\Override]
+	public function messageSuffix(): string
+	{
+		return 'is required.';
+	}
+}
+
+class InputBadFormatException extends InputException
+{
+	#[\Override]
+	public function messageSuffix(): string
+	{
+		return 'TODO';
+	}
+}
+
+class InputTooShortException extends InputException
+{
+	function __construct(public readonly int $minLength)
+	{}
+
+	#[\Override]
+	public function messageSuffix(): string
+	{
+		return "must be at least {$this->minLength} letters long.";
+	}
+}
+
+class InputTooLongException extends InputException
+{
+	function __construct(public readonly int $maxLength)
+	{}
+
+	#[\Override]
+	public function messageSuffix(): string
+	{
+		return "must be shorter than {$this->maxLength} letters.";
+	}
+}
+
+class InputMapFailedException extends InputException
+{
+	function __construct(
+		public readonly ?string $messageSuffix = null,
+		?\Throwable $previous = null
+	)
+	{
+		parent::__construct(previous: $previous);
+	}
+
+	#[\Override]
+	public function messageSuffix(): string
+	{
+		if ($this->messageSuffix !== null)
+		{
+			return $this->messageSuffix;
+		}
+
+		return 'doesn\'t look correct.';
 	}
 }
