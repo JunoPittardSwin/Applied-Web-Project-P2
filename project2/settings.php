2f400694e1c925122c952decf237c08118db268c
DissolvedK <39435125+DissolvedK@users.noreply.github.com>
2025-10-23 15:49:06 +1100

    feat: team_members table

--- project2/settings.php
+++ project2/settings.php
@@ -44,9 +44,42 @@ $db->execute_query("CREATE DATABASE IF NOT EXISTS $DB_NAME");
 $db->select_db($DB_NAME);
 
 // below: creates tables if they do not already exist
-// creates the contributions table
+// creates the jobs table
+$db->execute_query("CREATE TABLE IF NOT EXISTS jobs (
+	ref VARCHAR(5) NOT NULL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL,
+	salary_low INT NOT NULL,
+	salary_high INT NOT NULL,
+	reporting_line VARCHAR(50) NOT NULL,
+	about TEXT NOT NULL
+	);");
+
+// creates the jobs "essential requirements" sub-table
+$db->execute_query("CREATE TABLE IF NOT EXISTS jobs_ess_reqs (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	jobs_ref VARCHAR(5) NOT NULL,
+	ess_text TEXT NOT NULL,
+	FOREIGN KEY (jobs_ref) REFERENCES jobs(ref)
+	);");
+
+// creates the jobs "preferred requirements" sub-table
+$db->execute_query("CREATE TABLE IF NOT EXISTS jobs_pref_reqs (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	jobs_ref VARCHAR(5) NOT NULL,
+	pref_text TEXT NOT NULL,
+	FOREIGN KEY (jobs_ref) REFERENCES jobs(ref)
+	);");
+
+// creates the team_members table
+$db->execute_query("CREATE TABLE IF NOT EXISTS team_members (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(20) NOT NULL
+);");
+
+// creates the contributions sub-table
 $db->execute_query("CREATE TABLE IF NOT EXISTS contributions (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	team_member ENUM('Ashlyn', 'Juno', 'Aadil') NOT NULL,
-	contribution_text TEXT NOT NULL
+	team_member_id INT NOT NULL,
+	contribution_text TEXT NOT NULL,
+	FOREIGN KEY (team_member_id) REFERENCES team_members(id)
 	);");
