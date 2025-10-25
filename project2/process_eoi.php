6884e3c890bcc2c1f0dfd1e06a67463bb5a9470d
Ashlyn R <105928880@student.swin.edu.au>
2025-10-25 11:48:30 +1100

    fix: correct path to stylesheet

--- project2/process_eoi.php
+++ project2/process_eoi.php
@@ -24,7 +24,7 @@ function displayErrorPage(int $responseCode, callable $contentWriter): never
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Expression of Interest | Watertight CyberSec</title>
-		<link rel="stylesheet" href="./css/style.css">
+		<link rel="stylesheet" href="./styles/style.css">
 	</head>
 	<body>
 		<?php include(__DIR__ . '/header.inc'); ?>
